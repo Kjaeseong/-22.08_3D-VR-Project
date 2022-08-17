92da90a44e23bfc42c4b49d370c43fc13d7ca048
Kjaeseong <103081763+Kjaeseong@users.noreply.github.com>
2022-08-17 23:18:38 +0900

    Feat: 플레이어 애니메이션 적용

--- 22.08_3D,VR Project/Assets/Scripts/Player/PlayerAnimation.cs
+++ 22.08_3D,VR Project/Assets/Scripts/Player/PlayerAnimation.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class PlayerAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private PlayerStatus _player;
+    private Animator _anim;
+    private void Awake()
     {
-        
+        _player = GetComponentInParent<PlayerStatus>();
+        _anim = GetComponent<Animator>();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        _anim.SetBool("Move", _player.IsMoving);
+        _anim.SetBool("Run", _player.IsRunning);
     }
 }
